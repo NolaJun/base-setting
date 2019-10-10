be4f0bb37f789e9ee812ecb719fbf08c5afab059
Nola_jun <WuLiJun2116@163.com>
2019-10-10 17:18:04 +0800

    登录更新

--- src/Login/Index.vue
+++ src/Login/Index.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="login" :style="{height: height}">
     <van-cell-group class="middle-content border border-radius">
-      <h4 class="text-left">登录</h4><van-icon class="fas" class-prefix='fa' name='mobile-alt' />
+      <h4 class="text-left">登录</h4>
       <van-field class="form-input fas" border v-model="phone" label="手机号" left-icon="fa-mobile-alt" placeholder="请输入手机号码" error>
-        <van-image slot="left-icon"
-          width="20"
-          height="20"
-          src="../assets/img/mobile-alt.svg"
-        />
+        <van-image slot="left-icon" width="20" height="20" src="../assets/img/mobile-alt.svg"></van-image>
       </van-field>
       <van-field class="form-input" v-model="sms" center clearable label="验证码" placeholder="请输入短信验证码">
         <van-button class="form-input-btn" slot="button" size="small" type="primary">发送验证码</van-button>
